4abc2f325da354cf059e63147d178c87ecea525d
zbqyexingkong <zbqyexingkong@163.com>
2014-10-09 17:09:09 +0800

    宣传页控制页

---
+++ saveemail.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once ("autoload.php");
+
+if (isset($_POST['submit'])) {
+
+	$username = $_POST['username'];
+	$email = $_POST['email'];
+
+	if (!empty($username) && !empty($email)) {
+
+		$usereg = new savemail($username, $email);
+
+		$rest = $usereg->getResult();
+
+		//用户邮箱保存失败操作
+		if ($rest == false) {
+			echo '<script>alert("用户名已存在，请换个其他的用户名");window.history.go(-1);</script>';
+			exit;
+		} else {
+			echo '<script>window.location.href="#";</script>';//可将#号替换成需要跳转的页面地址
+		}
+
+	}
+
+}
+
+?>
