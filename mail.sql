ecef46d57b836768064de2e0c87b30926c6156c4
小宝 <autosend163@163.com>
2014-10-11 23:14:50 +0800

    内容修改，20141.10.11
    删去数据库中保存用户名项

--- mail.sql
+++ mail.sql
@@ -2,7 +2,6 @@ CREATE DATABASE IF NOT EXISTS pooker;
 USE pooker;
 CREATE TABLE IF NOT EXISTS useremail (
 	user_id		INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	name		varchar(250) not null,
 	mail		varchar(250) not null
 );
 
